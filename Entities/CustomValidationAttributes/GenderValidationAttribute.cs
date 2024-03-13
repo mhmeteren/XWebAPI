a8886093f9b91a513461c3fc75223caed6feab60
erenmehmett <mehmeter3n@gmail.com>
2024-03-13 14:44:26 +0300

    Add Custom Validation Attributes for DTOs

---
+++ Entities/CustomValidationAttributes/GenderValidationAttribute.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.CustomValidationAttributes
+{
+    internal class GenderValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] AllowedGenders = ["Female", "Male", "Other"];
+            string? gender = value?.ToString();
+            if (string.IsNullOrEmpty(gender) || !AllowedGenders.Contains(gender))
+            {
+                return new ValidationResult("Invalid gender value.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
